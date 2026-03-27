c98d082f8f5fbd3521fbd2c494172bb4b9f13f96
jack114 <1245836984@qq.com>
2026-03-27 16:11:14 +0800

    fix: configure edge externals to exclude node built-ins

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,6 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig();
+export default defineCloudflareConfig({
+    // 告诉构建器这些模块不应该被打包到 Workers bundle 中
+    edgeExternals: ["fs", "path", "os", "crypto", "stream", "buffer", "util"]
+});
